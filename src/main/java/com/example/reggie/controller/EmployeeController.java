c6392e6fc054d5e8fc89f9c5cf30342a1d5a3af0
eureka <eurekame2000@163.com>
2022-07-26 16:00:38 +0800

    add logout function

--- src/main/java/com/example/reggie/controller/EmployeeController.java
+++ src/main/java/com/example/reggie/controller/EmployeeController.java
@@ -69,4 +69,16 @@ public class EmployeeController {
         request.getSession().setAttribute("employee", employee.getId());
         return R.success(emp);
     }
+
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清理Session中保存的当前登陆员工的id
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
 }
