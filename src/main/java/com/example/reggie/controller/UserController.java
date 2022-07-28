7817234ced72705015793ffb77aa1590d3f43951
eureka <eurekame2000@163.com>
2022-07-28 23:48:22 +0800

    store code in redis instead of in session

--- src/main/java/com/example/reggie/controller/UserController.java
+++ src/main/java/com/example/reggie/controller/UserController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpSession;
 import java.security.GeneralSecurityException;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -26,6 +28,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Value("${reggie.email-from}")
     private String from;
@@ -56,7 +61,10 @@ public class UserController {
             new MailUtils().sendMail(code,from,to);
 
             //生成的验证码保存到session
-            session.setAttribute(email,code);
+            //session.setAttribute(email,code);
+
+            //将生成的验证码缓存到redis中，并设置有效期为5分钟
+            redisTemplate.opsForValue().set(email,code,5, TimeUnit.MINUTES);
 
             return R.success("邮箱验证码发送成功");
         }
@@ -81,7 +89,10 @@ public class UserController {
         //获取验证码
         String code = map.get("code").toString();
         //从Session中获取保存的验证码
-        Object codeInSession = session.getAttribute(email);
+        //Object codeInSession = session.getAttribute(email);
+
+        //从redis中获取缓存的验证码
+        Object codeInSession = redisTemplate.opsForValue().get(email);
 
         //进行验证码的比对（页面提交的验证码和Session中保存的验证码比对）
         if(codeInSession!=null&&codeInSession.equals(code)){
@@ -99,6 +110,9 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user",user.getId());
+            //如果用户登录成功，删除redis中缓存的验证码
+            redisTemplate.delete(email);
+
             return R.success(user);
         }
         return R.error("登录失败");
