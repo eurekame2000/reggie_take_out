1a1a869dfe4af90c8edf8c528aad8fee5c05c1fe
eureka <eurekame2000@163.com>
2022-07-30 19:52:02 +0800

    final version 2022/7/30

--- src/main/java/com/example/reggie/config/WebMvcConfig.java
+++ src/main/java/com/example/reggie/config/WebMvcConfig.java
@@ -1,18 +1,30 @@
 package com.example.reggie.config;
 
 import com.example.reggie.common.JacksonObjectMapper;
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 public class WebMvcConfig extends WebMvcConfigurationSupport {
+
     /**
      * 设置静态资源映射
      * @param registry
@@ -20,8 +32,11 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("进行静态资源映射...");
-        registry.addResourceHandler("backend/**").addResourceLocations("classpath:/backend/");
-        registry.addResourceHandler("front/**").addResourceLocations("classpath:/front/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
 
     /**
@@ -39,4 +54,24 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //index为零，优先使用我们自己的转换器
         converters.add(0,messageConverter);
     }
+
+    @Bean
+    public Docket createRestApi() {
+        // 文档类型
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.reggie.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("瑞吉外卖")
+                .version("1.0")
+                .description("瑞吉外卖接口文档")
+                .build();
+    }
+
 }
--- src/main/java/com/example/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/example/reggie/filter/LoginCheckFilter.java
@@ -38,7 +38,12 @@ public class LoginCheckFilter implements Filter {
                 "/front/**",
                 "/common/**",
                 "/user/sendMsg",//移动端发短信
-                "/user/login"//移动端登录
+                "/user/login",//移动端登录
+                "/doc.html",
+                "/webjars/**",
+                "/swagger-resources",
+                "/v2/api-docs",
+                "/favicon.ico"
         };
 
         //2.判断本次请求是否需要处理
